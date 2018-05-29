74082733effe1b6457809b35cf0881987b496ad3
Michael Kapelko <kornerr@gmail.com>
2018-05-29 18:57:01 +0300

    Add page controls

--- App/FourDirections/FourDirectionsView.swift
+++ App/FourDirections/FourDirectionsView.swift
@@ -9,8 +9,36 @@ class FourDirectionsView: UIView
     override func awakeFromNib()
     {
         super.awakeFromNib()
-
+        // On the left.
+        self.setupSectionPageControl()
+        // At the bottom.
+        self.setupCategoryPageControl()
     }
+    
+    // MARK: - SECTION PAGE CONTROL
 
+    @IBOutlet private var sectionPageControlContainerView: UIView!
+    private var sectionPageControl: UIPageControl!
+
+    private func setupSectionPageControl()
+    {
+        self.sectionPageControl = UIPageControl()
+        self.sectionPageControl.numberOfPages = 5
+        self.sectionPageControl.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 2.0)
+        self.sectionPageControlContainerView.embeddedView = self.sectionPageControl
+    }
+    
+    // MARK: - CATEGORY PAGE CONTROL
+    
+    @IBOutlet private var categoryPageControlContainerView: UIView!
+    private var categoryPageControl: UIPageControl!
+
+    private func setupCategoryPageControl()
+    {
+        self.categoryPageControl = UIPageControl()
+        self.categoryPageControl.numberOfPages = 5
+        self.categoryPageControlContainerView.embeddedView = self.categoryPageControl
+    }
+    
 }
 

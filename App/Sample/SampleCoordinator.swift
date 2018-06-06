93d83db72d9d9d0352b55fd4c3469d7fe2d916a1
Michael Kapelko <kornerr@gmail.com>
2018-06-06 15:15:15 +0300

    Make SampleView accept external layout function

--- App/Sample/SampleCoordinator.swift
+++ App/Sample/SampleCoordinator.swift
@@ -1,6 +1,14 @@
 
 import UIKit
 
+private let VIEWPORT_HEIGHT: CGFloat = 300
+
+private let ITEM_HEIGHT: CGFloat = 200
+private let ITEM_MIN_SIZE_FACTOR: CGFloat = 0.25
+private let ITEM_MAX_SIZE_FACTOR: CGFloat = 1.0
+private let ITEM_HEIGHT_COLLAPSED: CGFloat = ITEM_HEIGHT * ITEM_MIN_SIZE_FACTOR
+private let PAGE_SCROLL_SIZE = ITEM_HEIGHT_COLLAPSED
+
 class SampleCoordinator
 {
 
@@ -10,17 +18,21 @@ class SampleCoordinator
     var rootVCChanged: SimpleCallback?
 
     private var sampleView: SampleView!
-    private var sampleViewLayout02: SampleView!
 
     init()
     {
         // Create View and VC.
         self.sampleView = UIView.loadFromNib()
+        self.sampleView.setupScrolling(viewportHeight: VIEWPORT_HEIGHT)
+        self.sampleView.itemHeight = ITEM_HEIGHT
+        self.sampleView.itemHeightCollapsed = ITEM_HEIGHT_COLLAPSED
+
         let vc = UIViewControllerTemplate<SampleView>(mainView: self.sampleView)
         //let nc = UINavigationController(rootViewController: vc)
         self.rootVC = vc
 
         self.setupItems()
+        self.setupLayout()
     }
 
     private func setupItems()
@@ -35,8 +47,72 @@ class SampleCoordinator
             MItem("Accessories", color: .cyan),
             MItem("EDGE-BOTTOM", color: .gray),
         ]
-        self.sampleView.setItems(items)
+        self.sampleView.items = items
+    }
 
+    // MARK: - LAYING OUT
+
+    private func setupLayout()
+    {
+        self.sampleView.layoutReport = { [weak self] in
+            guard let this = self else { return }
+            //this.setupLayout01()
+            this.setupLayout02()
+        }
+    }
+
+    // MARK: - LAYOUT 01
+
+    private func setupLayout01()
+    {
+        // Scroll items.
+        self.sampleView.scrollingOffsetReport = { [weak self] in
+            guard let this = self else { return }
+            this.layItemsOut01()
+        }
+        // Perform the first laying out manually.
+        self.layItemsOut01()
+    }
+
+    private func layItemsOut01()
+    {
+        let offset = self.sampleView.scrollingOffset
+        let position = -offset / PAGE_SCROLL_SIZE
+        layViewsOut01(
+            views: self.sampleView.itemViews,
+            offset: 0,
+            position: position,
+            maxViewHeight: ITEM_HEIGHT,
+            minSizeFactor: ITEM_MIN_SIZE_FACTOR,
+            maxSizeFactor: ITEM_MAX_SIZE_FACTOR
+        )
+    }
+
+    // MARK: - LAYOUT 02
+
+    private func setupLayout02()
+    {
+        // Scroll items.
+        self.sampleView.scrollingOffsetReport = { [weak self] in
+            guard let this = self else { return }
+            this.layItemsOut02()
+        }
+        // Perform the first laying out manually.
+        self.layItemsOut02()
+    }
+
+    private func layItemsOut02()
+    {
+        let offset = self.sampleView.scrollingOffset
+        let position = -offset / PAGE_SCROLL_SIZE
+        layViewsOut02(
+            views: self.sampleView.itemViews,
+            offset: VIEWPORT_HEIGHT / 2.0 - ITEM_HEIGHT / 2.0, // Center of the view port.
+            position: position,
+            maxViewHeight: ITEM_HEIGHT,
+            minSizeFactor: ITEM_MIN_SIZE_FACTOR,
+            maxSizeFactor: ITEM_MAX_SIZE_FACTOR
+        )
     }
 
 }
--- App/Sample/SampleView.swift
+++ App/Sample/SampleView.swift
@@ -1,45 +1,29 @@
 
 import UIKit
 
-private let ITEM_HEIGHT: CGFloat = 200
-private let ITEM_MIN_SIZE_FACTOR: CGFloat = 0.25
-private let ITEM_MAX_SIZE_FACTOR: CGFloat = 1.0
-private let ITEM_HEIGHT_COLLAPSED: CGFloat = ITEM_HEIGHT * ITEM_MIN_SIZE_FACTOR
-
-private let VIEWPORT_HEIGHT: CGFloat = 300
-private let PAGE_SCROLL_SIZE: CGFloat = ITEM_HEIGHT_COLLAPSED
-
 class SampleView: UIView
 {
 
-    // MARK: - SETUP
-    
-    @IBOutlet private var gestureView: UIView!
-    
-    override func awakeFromNib()
-    {
-        super.awakeFromNib()
-        self.setupScrolling()
-    }
-
-    override func layoutSubviews()
-    {
-        super.layoutSubviews()
-        self.generateItemViews()
-        //self.setupItemsLayout01()
-        self.setupItemsLayout02()
-    }
-
     // MARK: - SCROLLING
 
-    var scrolling: Scrolling!
-    var scrollingBounds: ScrollingBounds!
-    
-    private func setupScrolling()
+    @IBOutlet private var gestureView: UIView!
+    private var scrolling: Scrolling!
+    private var scrollingBounds: ScrollingBounds!
+
+    var scrollingOffsetReport: SimpleCallback?
+    var scrollingOffset: CGFloat
+    {
+        get
+        {
+            return self.scrollingBounds.contentOffset
+        }
+    }
+
+    func setupScrolling(viewportHeight: CGFloat)
     {
         self.scrolling = Scrolling(trackedView: self.gestureView)
         self.scrollingBounds = ScrollingBounds()
-        self.scrollingBounds.viewportHeight = VIEWPORT_HEIGHT
+        self.scrollingBounds.viewportHeight = viewportHeight
         self.scrolling.verticalReport = { [weak self] in
             guard let this = self else { return }
             this.scrollingBounds.setContentOffset(delta: this.scrolling.verticalDelta)
@@ -48,31 +32,28 @@ class SampleView: UIView
         self.scrolling.verticalFinishReport = {
         }
         */
+
+        // Relay scrolling report.
+        self.scrollingBounds.contentOffsetReport = { [weak self] in
+            guard let this = self else { return }
+            if let report = this.scrollingOffsetReport
+            {
+                report()
+            }
+        }
     }
 
     // MARK: - ITEMS
 
-    private var items = [MasterItem]()
-
-    // NOTE Only call this function before first display.
-    func setItems(_ items: [MasterItem])
-    {
-        self.items = items
-        self.scrollingBounds.contentHeight = VIEWPORT_HEIGHT + ITEM_HEIGHT_COLLAPSED * CGFloat(items.count - 1)
-    }
+    // NOTE Set BEFORE first display.
+    var items = [MasterItem]()
+    // NOTE Set BEFORE first display.
+    var itemHeight: CGFloat = 0
+    // NOTE Set BEFORE first display.
+    var itemHeightCollapsed: CGFloat = 0
 
     @IBOutlet private var itemsView: UIView!
-
-    // MARK: - ITEM VIEWS
-    
-    private var itemViews = [UIView]()
-
-    private func generateItemViews()
-    {
-        let width = self.frame.size.width
-        let size = CGSize(width: width, height: ITEM_HEIGHT)
-        self.generateItemViews(for: self.items, withSize: size)
-    }
+    var itemViews = [UIView]()
 
     private func generateItemViews(for items: [MasterItem], withSize size: CGSize)
     {
@@ -94,58 +75,35 @@ class SampleView: UIView
         }
     }
 
-    // MARK: - LAYOUT 01
+    // MARK: - LAYING OUT
 
-    private func setupItemsLayout01()
+    var layoutReport: SimpleCallback?
+
+    override func layoutSubviews()
     {
-        // Scroll items.
-        self.scrollingBounds.contentOffsetReport = { [weak self] in
-            guard let this = self else { return }
-            this.layItemsOut01()
+        super.layoutSubviews()
+
+        self.updateScrollingRange()
+
+        // Generate item views.
+        let width = self.frame.size.width
+        let size = CGSize(width: width, height: self.itemHeight)
+        self.generateItemViews(for: self.items, withSize: size)
+
+        // Invoke external layout setup.
+        if let report = self.layoutReport
+        {
+            report()
         }
-        // Perform the first laying out manually.
-        self.layItemsOut01()
     }
 
-    private func layItemsOut01()
+    private func updateScrollingRange()
     {
-        let offset = self.scrollingBounds.contentOffset
-        let position = -offset / PAGE_SCROLL_SIZE
-        layViewsOut01(
-            views: self.itemViews,
-            offset: 0,
-            position: position,
-            maxViewHeight: ITEM_HEIGHT,
-            minSizeFactor: ITEM_MIN_SIZE_FACTOR,
-            maxSizeFactor: ITEM_MAX_SIZE_FACTOR
-        )
+        // Make sure we can scroll through each item.
+        self.scrollingBounds.contentHeight =
+            self.scrollingBounds.viewportHeight +
+            self.itemHeightCollapsed * CGFloat(self.items.count - 1)
     }
 
-    // MARK: - LAYOUT 02
-
-    private func setupItemsLayout02()
-    {
-        // Scroll items.
-        self.scrollingBounds.contentOffsetReport = { [weak self] in
-            guard let this = self else { return }
-            this.layItemsOut02()
-        }
-        // Perform the first laying out manually.
-        self.layItemsOut02()
-    }
-
-    private func layItemsOut02()
-    {
-        let offset = self.scrollingBounds.contentOffset
-        let position = -offset / PAGE_SCROLL_SIZE
-        layViewsOut02(
-            views: self.itemViews,
-            offset: VIEWPORT_HEIGHT / 2.0 - ITEM_HEIGHT / 2.0, // Center of the view port.
-            position: position,
-            maxViewHeight: ITEM_HEIGHT,
-            minSizeFactor: ITEM_MIN_SIZE_FACTOR,
-            maxSizeFactor: ITEM_MAX_SIZE_FACTOR
-        )
-    }
 }
 
